1a484e4acb70b51eaa54ec714d5c6502d28d64c0
OMAKHMUDOFF <omaxmduovv@gmail.com>
2024-01-10 11:33:41 +0500

    tailwind responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 function App() {
   return (
     <>
-      <h1 className="text-2xl text-blue-700 font-bold md: text-xl text-red-500">
+      <h1 className="text-2xl text-blue-700 font-bold md:text-xl text-red-500">
         Hello world
       </h1>
     </>
